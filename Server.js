bc0325ba8aeb1346319c7900acf643e72a02905f
Saurabhh Tyagi <styagi689@gmail.com>
2020-07-15 11:38:44 +0530

    init commit

--- Server.js
+++ Server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use("/gradients", require("./routes/api/gradient"));
 
-if (process.env.NODE_ENV == "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
